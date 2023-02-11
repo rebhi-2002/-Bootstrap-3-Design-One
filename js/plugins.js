3e4c87a5db4debd3b4351de3f47a00a6a3e0755a
Rebhe <rebheibrahim@gmail.com>
2023-02-11 05:24:12 +0200

    Upload The Design

---
+++ js/plugins.js
@@ -0,0 +1,94 @@
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+  "use strict";
+
+  // Nice Scroll
+
+  $("html").niceScroll();
+
+  $(".carousel").carousel({
+    interval: 6000,
+  });
+
+  // Show Color Option Div When Click On The Gear
+
+  $(".gear-check").click(function () {
+    // $(".color-option").show();
+    // $(".color-option").toggle();
+    $(".color-option").fadeToggle();
+  });
+
+  // Change Theme Color On Click
+
+  var colorLi = $(".color-option ul li"),
+    scrollButton = $("#scroll-top");
+
+  colorLi
+    .eq(0)
+    .css("backgroundColor", "#E60024")
+    .end()
+
+    .eq(1)
+    .css("backgroundColor", "#E426D5")
+    .end()
+
+    .eq(2)
+    .css("backgroundColor", "#009AFF")
+    .end()
+
+    .eq(3)
+    .css("backgroundColor", "#FFD500");
+
+  colorLi.click(function () {
+    $("link[href*='theme']").attr("href", $(this).attr("data-value"));
+    // console.log($(this).attr("data-value"));
+  });
+
+  // Caching The Scroll Top Element
+
+  $(window).scroll(function () {
+    // console.log($(this).scrollTop());
+    if ($(this).scrollTop() >= 700) {
+      scrollButton.show();
+    } else {
+      scrollButton.hide();
+    }
+    // $(this).scrollTop() >= 700 ? scrollButton.show() : scrollButton.hide();
+  });
+
+  // Click On Button To Scroll Top
+
+  scrollButton.click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 600);
+  });
+});
+
+// Loading Screen
+
+$(window).load(function () {
+  "use strict";
+
+  // Loading Elements
+
+  $(".loading-overlay .spinner").fadeOut(2000, function () {
+    // Show The Scroll
+
+    $("body").css("overflow", "auto");
+
+    $(this)
+      .parent()
+      .fadeOut(2000, function () {
+        $(this).remove();
+      });
+  });
+
+  // or: $(".loading-overlay, .loading-overlay .spinner").fadeOut(2000);
+});
+
+// FAQ Accordion
+
+document.querySelector(".faq-questions .panel-heading a.in").onclick =
+  function () {
+    this.classList.toggle("in");
+  };
